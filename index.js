a585c24ace94992bec568b30c324ed94001d14a8
Lauren <lauren.noble3@gmail.com>
2023-05-04 13:44:24 -0400

    linked index.js script, added buttons, styling

---
+++ index.js
@@ -0,0 +1,7 @@
+let card1 = document.getElementById("card1")
+let card2 = document.getElementById("card2")
+let card3 = document.getElementById("card3")
+let card4 = document.getElementById("card4")
+let card5 = document.getElementById("card5")
+let card6 = document.getElementById("card6")
+
