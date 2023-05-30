84084d5147c2473de8c5e69a1f47a2f57117fc8c
Lauren <lauren.noble3@gmail.com>
2023-05-30 17:24:55 -0400

    updated input event listener, added guess array, shows game over message when guess count is 6

--- index.js
+++ index.js
@@ -1,70 +1,85 @@
-let card1 = document.getElementById("card1");
-let card2 = document.getElementById("card2");
-let card3 = document.getElementById("card3");
-let card4 = document.getElementById("card4");
-let card5 = document.getElementById("card5");
-let card6 = document.getElementById("card6");
+let card1 = document.getElementById('card1').id;
+let card2 = document.getElementById('card2').id;
+let card3 = document.getElementById('card3').id;
+let card4 = document.getElementById('card4').id;
+let card5 = document.getElementById('card5').id;
+let card6 = document.getElementById('card6').id;
 
-const length3 = document.getElementById("length3");
-const length4 = document.getElementById("length4");
-const length5 = document.getElementById("length5");
-const length6 = document.getElementById("length6");
-const length7 = document.getElementById("length7");
-const length8 = document.getElementById("length8");
-const length9 = document.getElementById("length9");
+const length3 = document.getElementById('length3');
+const length4 = document.getElementById('length4');
+const length5 = document.getElementById('length5');
+const length6 = document.getElementById('length6');
+const length7 = document.getElementById('length7');
+const length8 = document.getElementById('length8');
+const length9 = document.getElementById('length9');
 
-const cardArray = [card1, card2, card3, card4, card5, card6];
+const guessArray = [card1, card2, card3, card4, card5, card6];
 
 // input event listener
-let input = document.querySelector("input");
-input.addEventListener("input", (event) => {
+let input = document.querySelector('input');
+let guessCount = 0;
+input.addEventListener('input', (event) => {
+  guessCount++;
+  if (guessCount === 6) {
+    gameContainer.innerHTML = 'Sorry, game over.';
+  }
+  for (let i = 0; i < guessArray.length; i++) {
+    if (guessArray[i].length > 1) {
+      guessArray[i] = event.target.value;
+      break;
+    }
+  }
+});
 
-    card1.textContent = event.target.value;
-})
-
-
-const lengthArray = [length3, length4, length5, length6, length7, length8, length9];
+const lengthArray = [
+  length3,
+  length4,
+  length5,
+  length6,
+  length7,
+  length8,
+  length9,
+];
 let wordLength = 6;
 
 lengthArray.forEach((length) => {
-    length.addEventListener("click", () => {
-        wordLength = length.innerHTML;
-    })
+  length.addEventListener('click', () => {
+    wordLength = length.innerHTML;
+  });
 });
 
+const startButton = document.getElementById('startbtn');
+const resetButton = document.getElementById('resetbtn');
+const quitButton = document.getElementById('quitbtn');
 
-const startButton = document.getElementById("startbtn");
-const resetButton = document.getElementById("resetbtn");
-const quitButton = document.getElementById("quitbtn");
-
-let gameContainer = document.getElementById("game");
-let word = "";
+let gameContainer = document.getElementById('game');
+let word = '';
 
 // gets random word with length
 async function getRandomWord() {
-    const response = await fetch(`https://random-word-api.vercel.app/api?words=1&length=${wordLength}`);
-    const data = await response.json();
-    word = data[0];
-    gameContainer.innerHTML = word;
+  const response = await fetch(
+    `https://random-word-api.vercel.app/api?words=1&length=${wordLength}`
+  );
+  const data = await response.json();
+  word = data[0];
+  gameContainer.innerHTML = word;
 }
 
 // button event listeners
-startButton.addEventListener("click", () => {
-    gameContainer.innerHTML = ""
-    getRandomWord();
+startButton.addEventListener('click', () => {
+  gameContainer.innerHTML = '';
+  getRandomWord();
 });
 
-resetButton.addEventListener("click", () => {
-    gameContainer.innerHTML = ""
-    for(let i = 0; i < cardArray.length; i++) {
-        let currendCard = cardArray[i]
-        currendCard.innerHTML = ""
-    }
+resetButton.addEventListener('click', () => {
+  gameContainer.innerHTML = '';
+  for (let i = 0; i < cardArray.length; i++) {
+    let currendCard = cardArray[i];
+    currendCard.innerHTML = '';
+  }
 });
 
-quitButton.addEventListener("click", () => {
-    gameContainer.innerHTML = "Thanks for playing.";
-    gameContainer.style.color = "red";
+quitButton.addEventListener('click', () => {
+  gameContainer.innerHTML = 'Thanks for playing.';
+  gameContainer.style.color = 'red';
 });
-
-
